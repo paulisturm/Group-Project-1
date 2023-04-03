5028b54d829a585fce42a7f8ee7c530da2ed3df5
Seth <sandregg2gmail.com>
2023-04-03 19:01:49 -0400

    characters

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
-var key = '3fcb4e8f7a5e36f86e0c9ba1dc7048ea'
+    //console.log(passhash);
+
+      var apikey = "324637569c8fda463203df55e0b15edc";
+
+      var d = new Date();
+      var dstr = d.getTime();
+      var pub = "324637569c8fda463203df55e0b15edc";
+      var pri = "e0fd8fcb37f122d05eec3fe26aa303e32453c601";
+      var combinedStr = dstr + pri + pub;
+      var passhash = CryptoJS.MD5(combinedStr).toString();
+      //console.log(combinedStr, passhash);
+      var url = `https://gateway.marvel.com:443/v1/public/characters?ts=${dstr}&apikey=${apikey}&hash=${passhash}`;
+      fetch(url)
+        .then((res) => {
+          console.log(res);
+          return res.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
