cb000c8ed09701cee8651b6f22dd1d567e261a1d
Seth <sandregg2gmail.com>
2023-04-05 19:07:48 -0400

    Favourites page with new buttons

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,53 @@
 //console.log(passhash);
-      var apikey = "324637569c8fda463203df55e0b15edc";
+var apikey = "324637569c8fda463203df55e0b15edc";
 
-      var d = new Date();
-      var dstr = d.getTime();
-      var pub = "324637569c8fda463203df55e0b15edc";
-      var pri = "e0fd8fcb37f122d05eec3fe26aa303e32453c601";
-      var combinedStr = dstr + pri + pub;
-      var passhash = CryptoJS.MD5(combinedStr).toString();
-      //console.log(combinedStr, passhash);
+var d = new Date();
+var dstr = d.getTime();
+var pub = "324637569c8fda463203df55e0b15edc";
+var pri = "e0fd8fcb37f122d05eec3fe26aa303e32453c601";
+var combinedStr = dstr + pri + pub;
+var passhash = CryptoJS.MD5(combinedStr).toString();
+//console.log(combinedStr, passhash);
 
 
-      var url = `https://gateway.marvel.com:443/v1/public/characters?ts=${dstr}&apikey=${apikey}&hash=${passhash}`;
+var url = `https://gateway.marvel.com:443/v1/public/characters?ts=${dstr}&apikey=${apikey}&hash=${passhash}`;
 
-      var secondapikey = "6813105332038237";
+var secondapikey = "6813105332038237";
 
-      var secondurl = `https://www.superheroapi.com/api.php/6813105332038237/search/${name}`;
+var secondurl = `https://www.superheroapi.com/api.php/6813105332038237/search/${name}`;
 
-      
-      fetch(url)
-        .then((res) => {
-          console.log(res);
-          return res.json();
-        })
-        .then((json) => {
-          console.log(json);
-        });
-<<<<<<< HEAD
-=======
 
-        fetch(secondurl)
-        .then((res) => {
-            console.log(res);
-            return res.json();
-          })
-          .then((json) => {
-            console.log(json);
-          });
+fetch(url)
+  .then((res) => {
+    console.log(res);
+    return res.json();
+  })
+  .then((json) => {
+    console.log(json);
+  });
 
-        function toggleActiveState() {
-            this.classList.toggle('active');
-        }
-        var btns = document.querySelectorAll('.btn');
-        [].forEach.call(btns, function(btn) {
-          btn.addEventListener('click', toggleActiveState, false);
-          
-        });
-        
+  fetch(secondurl)
+  .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json);
+    });
 
-    //    () => {
-           // var character = document.getElementById('search').value
-          //  fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${dstr}&apikey=${apikey}&hash=${passhash}` + results +"results")
-         //   .then(res=> res.json())
-   // .then(result=> console.log("result", result))
+  function toggleActiveState() {
+      this.classList.toggle('active');
+  }
+  var btns = document.querySelectorAll('.btn');
+  [].forEach.call(btns, function(btn) {
+    btn.addEventListener('click', toggleActiveState, false);
     
->>>>>>> f2de73052e3c215355210e817328e079c917ef43
+  });
+  
+
+//    () => {
+     // var character = document.getElementById('search').value
+    //  fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${dstr}&apikey=${apikey}&hash=${passhash}` + results +"results")
+   //   .then(res=> res.json())
+// .then(result=> console.log("result", result))
+
